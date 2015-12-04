3aa3b25934f144425f0406993f9112011ad82d11
RomaBlaSo <voloshyn.roman@gmail.com>
2015-12-05 00:53:07 +0200

    05.12.2015 00:30

--- src/MyUtils/StringUtils.java
+++ src/MyUtils/StringUtils.java
@@ -5,9 +5,33 @@ package myUtils;
  */
 public class StringUtils {
 
-    public static void hello(String name){
+    public static void hello(String name) {
 
         System.out.println("Hello, " + name);
+
+    }
+
+    public static String kopeks(int sum) {
+
+        String kopeks = "";
+        if (sum >= 5 && sum <= 20) {
+            kopeks = "копеек";
+        } else {
+            int d = sum % 10;
+            kopeks = d == 1 ? "копейка" : d <= 4 ? "копейки" : "копеек";
+        }
+
+        return kopeks;
+
+        //if(number > 20)
+//        number %= 10;
+//        switch(number)
+//        {
+//        case 1: std::cout << "копейка" << std::endl; break;
+//        case 2:case 3: case 4: std::cout << "копейки " << std::endl; break;
+//        case 5: case 6: case 7: case 8: case 9: case 10: case 11: case 12:
+//        case 13: case 14: case 15: case 16: case 17:case 18: case 19:case 20:
+//        std::cout << "копеек " << std::endl; break;
 
     }
 
