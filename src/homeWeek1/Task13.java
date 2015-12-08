ebdfb2d498693a7d92915b01cdfc8046e50aa29b
RomaBlaSo <voloshyn.roman@gmail.com>
2015-12-08 23:16:02 +0200

    hw 1

--- src/homeWeek1/Task13.java
+++ src/homeWeek1/Task13.java
@@ -1,7 +1,5 @@
 package homeWeek1;
 
-import myUtils.ArrayUtils;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
--- src/homeWeek1/Task14_.java
+++ src/homeWeek1/Task14_.java
@@ -1,7 +1,36 @@
 package homeWeek1;
 
-/**
- * Created by roma on 04.12.2015.
- */
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task14_ {
+
+    public static void main(String[] args) {
+
+        //Дано натуральное число A > 1. Определите, каким по счету числом Фибоначчи оно является, то есть выведите такое число n, что φn=A.
+        //Если А не является числом Фибоначчи, выведите число -1.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter count:");
+        int count = sc.nextInt();
+
+        int[] numbers = new int[count + 1];
+        if (count < 2) {
+            System.out.println(count);
+            return;
+        } else {
+            numbers[0] = 0;
+            numbers[1] = 1;
+        }
+
+        for (int i = 2; i <= count; i++) {
+
+            numbers[i] = numbers[i - 1] + numbers[i - 2];
+
+        }
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
 }
